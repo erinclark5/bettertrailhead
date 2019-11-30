4e21eeb714c8cc4e7b59e4f8aaea5a9e512c8c5c
Seifeldin Dabbour <swdabbour@gmail.com>
2019-11-29 19:57:10 -0700

    Added sql file

--- trailhead.sql
+++ trailhead.sql
@@ -1,40 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2019 at 03:56 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
 
-CREATE TABLE IF NOT EXISTS USERS (
-  uname varchar(255),
-  pass varchar(255),
-  email varchar(255),
-  id int,
-  PRIMARY KEY(uname, pass, email, id)
-);
-
-
-CREATE TABLE IF NOT EXISTS ADMINISTRATION (
-  adminid int,
-  firstname varchar(255) NOT NULL,
-  lastname varchar(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS COURSES (
-  courseid varchar(255),
-  coursename varchar(255),
-  PRIMARY KEY(courseid)
-);
-
-CREATE TABLE IF NOT EXISTS SCHEDULES (
-  studentid int NOT NULL,
-  courseid varchar(255),
-  PRIMARY KEY(studentid, courseid)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE IF NOT EXISTS STUDENTS (
-  studentid int(11) NOT NULL,
-  firstname text NOT NULL,
-  lastname text NOT NULL,
-  PRIMARY KEY(studentid)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO COURSES VALUES 
-("CSCI406","Algorithms"),
-("CSCI441","Computer Graphics"),
-("CSCI445","Web Programming");
+--
+-- Database: `trailhead`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `Alias` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `Alias`) VALUES
+(1001, 'Sword'),
+(1296, 'SeifeldinDabbour');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `administration`
+--
+
+CREATE TABLE `administration` (
+  `adminid` int(11) DEFAULT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `courseid` varchar(7) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`courseid`, `name`) VALUES
+('CSCI261', 'Programming Concepts'),
+('CSCI262', 'Data Structures'),
+('CSCI406', 'Algorithms'),
+('CSCI441', 'Computer Graphics'),
+('CSCI445', 'Web Programming'),
+('PHGN100', 'Physics 1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedules`
+--
+
+CREATE TABLE `schedules` (
+  `userid` int(11) NOT NULL,
+  `courseid` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `schedules`
+--
+
+INSERT INTO `schedules` (`userid`, `courseid`) VALUES
+(1001, 'CSCI261'),
+(1001, 'PHGN100');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `first` text NOT NULL,
+  `last` text NOT NULL,
+  `email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `first`, `last`, `email`) VALUES
+(1011, 'Seifeldin', 'Dabbour', 'swdab@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uname` text NOT NULL,
+  `password` text NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uname`, `password`, `id`) VALUES
+('swdab', 'dabby', 1001),
+('student', 'pass', 1011),
+('daswod', 'dabby', 1296);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`courseid`);
+
+--
+-- Indexes for table `schedules`
+--
+ALTER TABLE `schedules`
+  ADD UNIQUE KEY `studentid` (`userid`,`courseid`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD KEY `CWID` (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `uname` (`uname`,`password`,`id`) USING HASH;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `ACWID` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `CWID` FOREIGN KEY (`id`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
